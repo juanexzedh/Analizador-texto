c854e1d13cb10f60d4319056af059df1baee80ef
Juanex <juanexzedh@gmail.com>
2026-07-04 15:30:28 -0500

    Se regreso a usar Textblob debido al consumo constante de memoria de transformers

--- analisis/prueba.py
+++ analisis/prueba.py
@@ -1,11 +1,21 @@
-from transformers import pipeline
+from textblob import TextBlob
+from deep_translator import GoogleTranslator
 
-analizador = pipeline(
-    "sentiment-analysis",
-    model="pysentimiento/robertuito-sentiment-analysis"
-)
-texto = "Me encanta este celular, funciona de maravilla."
+#texto = "I love my mom, she always makes me happy. I am proud that I have her in my life"
+# analisis = TextBlob(texto)
 
-resultado = analizador(texto)
-print(texto)
-print(resultado)
+textos = [
+    "Me encanta este celular. Es lo mejor que me ha pasado en la vida, lo amo.",
+    "Odio hacer filas en el banco.",
+    "Hoy está lloviendo.",
+    "Detesto que me traten asi. Todos son malos conmigo, nadie me quiere, todos me odian.",
+    "El examen fue difícil."
+]
+
+for texto in textos:
+    traduccion = GoogleTranslator(source='es', target='en').translate(texto)
+    analisis = TextBlob(traduccion)
+    print(f"Texto: {traduccion}")
+    print(f"Polaridad: {analisis.sentiment.polarity}")
+    print(f"Subjetividad: {analisis.sentiment.subjectivity}")
+    print("-" * 40)
--- analisis/sentiment.py
+++ analisis/sentiment.py
@@ -1,2 +1,25 @@
 from textblob import TextBlob
 from deep_translator import GoogleTranslator
+
+def analizador_sentimiento(texto):
+    traduccion = GoogleTranslator(source='es', target='en').translate(texto)
+    analisis = TextBlob(traduccion)
+
+    pol = analisis.sentiment.polarity
+    sub = analisis.sentiment.subjectivity
+    if pol >= 0.2:
+        clasificacion = "Positivo"
+    elif pol <= 0.2:
+        clasificacion = "Negativo"
+    else:
+        clasificacion = "Neutro"
+
+    resultados = {"score": pol, "clasificacion": clasificacion, "sub": sub}
+
+    print(f"Texto: {traduccion}")
+    print(f"Polaridad: {pol}")
+    print(f"Subjetividad: {sub}")
+    print(f"Clasificacion: {clasificacion}")
+
+    return resultados
+    
